d9b15da5ee0af9cc516582468e1cd26f2405e49a
alex <k2.bralex@gmail.com>
2023-03-14 10:25:18 +0300

    update crud func

--- web/crud/main.go
+++ web/crud/main.go
@@ -44,80 +44,85 @@ func main() {
 		return c.SendStatus(200)
 	})
 
-	webApp.Post("/tasks", func(ctx *fiber.Ctx) error {
-		var create CreateTaskRequest
-		err := ctx.BodyParser(&create)
-		if err != nil {
-			return err
-		}
-
-		tasks[taskIDCounter] = Task{
-			ID:       taskIDCounter,
-			Desc:     create.Desc,
-			Deadline: create.Deadline,
-		}
-		taskIDCounter++
-
-		ctx.Status(fiber.StatusOK)
-		return ctx.JSON(CreateTaskResponse{ID: taskIDCounter - 1})
-	})
-
-	webApp.Patch("/tasks/:id", func(ctx *fiber.Ctx) error {
-		var update UpdateTaskRequest
-		err := ctx.BodyParser(&update)
-		if err != nil {
-			return ctx.SendStatus(fiber.StatusBadRequest)
-		}
-
-		id, err := ctx.ParamsInt("id")
-		if err != nil {
-			return ctx.SendStatus(fiber.StatusBadRequest)
-		}
-
-		task, ok := tasks[int64(id)]
-		if !ok {
-			return ctx.SendStatus(fiber.StatusNotFound)
-		}
-
-		task.Desc = update.Desc
-		task.Deadline = update.Deadline
-		tasks[task.ID] = task
-
-		return ctx.SendStatus(fiber.StatusOK)
-	})
-
-	webApp.Get("/tasks/:id", func(ctx *fiber.Ctx) error {
-		id, err := ctx.ParamsInt("id")
-		if err != nil {
-			return ctx.SendStatus(fiber.StatusBadRequest)
-		}
-
-		task, ok := tasks[int64(id)]
-		if !ok {
-			return ctx.SendStatus(fiber.StatusNotFound)
-		}
-
-		ctx.Status(fiber.StatusOK)
-		return ctx.JSON(GetTaskResponse{
-			ID:       task.ID,
-			Desc:     task.Desc,
-			Deadline: task.Deadline,
-		})
-	})
-
-	webApp.Delete("/tasks/:id", func(ctx *fiber.Ctx) error {
-		id, err := ctx.ParamsInt("id")
-		if err != nil {
-			return ctx.SendStatus(fiber.StatusBadRequest)
-		}
-
-		if _, ok := tasks[int64(id)]; !ok {
-			return ctx.SendStatus(fiber.StatusNotFound)
-		}
-
-		delete(tasks, int64(id))
-		return ctx.SendStatus(fiber.StatusOK)
-	})
+	webApp.Post("/tasks", create)
+	webApp.Patch("/tasks/:id", update)
+	webApp.Get("/tasks/:id", read)
+	webApp.Delete("/tasks/:id", delete)
 
 	logrus.Fatal(webApp.Listen(":8080"))
+}
+
+func create(ctx *fiber.Ctx) error {
+	var create CreateTaskRequest
+	err := ctx.BodyParser(&create)
+	if err != nil {
+		return err
+	}
+
+	tasks[taskIDCounter] = Task{
+		ID:       taskIDCounter,
+		Desc:     create.Desc,
+		Deadline: create.Deadline,
+	}
+	taskIDCounter++
+
+	ctx.Status(fiber.StatusOK)
+	return ctx.JSON(CreateTaskResponse{ID: taskIDCounter - 1})
+}
+
+func update(ctx *fiber.Ctx) error {
+	var update UpdateTaskRequest
+	err := ctx.BodyParser(&update)
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	id, err := ctx.ParamsInt("id")
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	task, ok := tasks[int64(id)]
+	if !ok {
+		return ctx.SendStatus(fiber.StatusNotFound)
+	}
+
+	task.Desc = update.Desc
+	task.Deadline = update.Deadline
+	tasks[task.ID] = task
+
+	return ctx.SendStatus(fiber.StatusOK)
+}
+
+func read(ctx *fiber.Ctx) error {
+	id, err := ctx.ParamsInt("id")
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	task, ok := tasks[int64(id)]
+	if !ok {
+		return ctx.SendStatus(fiber.StatusNotFound)
+	}
+
+	ctx.Status(fiber.StatusOK)
+	return ctx.JSON(GetTaskResponse{
+		ID:       task.ID,
+		Desc:     task.Desc,
+		Deadline: task.Deadline,
+	})
+}
+
+func delete(ctx *fiber.Ctx) error {
+	id, err := ctx.ParamsInt("id")
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	if _, ok := tasks[int64(id)]; !ok {
+		return ctx.SendStatus(fiber.StatusNotFound)
+	}
+
+	delete(tasks, int64(id))
+	return ctx.SendStatus(fiber.StatusOK)
 }

d6bc69d0118cc64afe7efd78c9172bcf40eb5112
alex <k2.bralex@gmail.com>
2023-03-14 18:35:08 +0300

    add model.go

---
+++ web/crud/model.go
@@ -0,0 +1,29 @@
+package main
+
+type (
+	GetTaskResponse struct {
+		ID       int64  `json:"id"`
+		Desc     string `json:"description"`
+		Deadline int64  `json:"deadline"`
+	}
+
+	CreateTaskRequest struct {
+		Desc     string `json:"description"`
+		Deadline int64  `json:"deadline"`
+	}
+
+	CreateTaskResponse struct {
+		ID int64 `json:"id"`
+	}
+
+	UpdateTaskRequest struct {
+		Desc     string `json:"description"`
+		Deadline int64  `json:"deadline"`
+	}
+
+	Task struct {
+		ID       int64
+		Desc     string
+		Deadline int64
+	}
+)
